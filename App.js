92403cffe6a2d19b0f9f799453432327b7bfddfb
pramodkavi <pramodkavindu44@gmail.com>
2023-08-07 08:47:17 +0530

    ignre authprovider

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { AuthProvider } from "./Context/AuthContext";
 import AppNav from "./navigation/AppNav";
 import React from "react";
 import { store } from "./store/store";
+import {Navigation} from "./navigation/navigation";
 export default function App() {
   return (
     <Provider store={store}>
