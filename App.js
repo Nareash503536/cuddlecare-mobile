c8846d36d43cfe9974d7dbd681c2e592704f2320
pramodkavi <pramodkavindu44@gmail.com>
2023-08-16 20:48:44 +0530

    just edit

--- App.js
+++ App.js
@@ -12,9 +12,10 @@ export default function App() {
 
     return (
         <Provider store={store}>
-            <AuthProvider>
-                <AppNav />
-            </AuthProvider>
+            {/*<AuthProvider>*/}
+            {/*    <AppNav />*/}
+            {/*</AuthProvider>*/}
+            <Navigation/>
         </Provider>
     );
 }
