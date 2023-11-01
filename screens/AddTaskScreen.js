7b6da86451a0f4c5a4848442f2a15dc37edcb915
Kushantha <kushantha018@gmail.com>
2023-11-01 10:02:56 +0530

    Add Task Screen

--- screens/AddTaskScreen.js
+++ screens/AddTaskScreen.js
@@ -1,20 +1,45 @@
 import {SafeAreaView} from "react-native-safe-area-context";
-import {View, Text, Modal, TextInput, Pressable, Alert} from "react-native";
+import {View, Text, Modal, TextInput, Pressable, Alert, TouchableOpacity, FlatList} from "react-native";
 import {useRoute} from "@react-navigation/core";
 import FilledButton from "../components/filledButton";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ToDoHeader from "../components/toDoHeader";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import moment from "moment/moment";
+import {BASE_URL} from "../config";
+import axios from "axios";
+import {AuthContext} from "../Context/AuthContext";
 
 export function AddTaskScreen(){
+    const { updateKeys } = React.useContext(AuthContext);
     const route = useRoute();
     const date = route.params?.date || {};
+    const parsedDate = new Date(date);
+    const formattedDate = parsedDate.toISOString();
+
     const [modalVisible, setModalVisible] = useState(false);
     const [task, setTask] = useState('');
     const [description, setDescription] = useState('');
     const [time, setTime] = useState('');
     const [isTimePickerVisible, setTimePickerVisible] = useState(false);
+    const [toDos, setToDos] = useState([]);
+
+    useEffect(() => {
+        getAllTasks();
+    }, []);
+
+    const getAllTasks = async () => {
+        const apiURL = BASE_URL + "/api/todolist/getAll/" + formattedDate;
+        try {
+            await updateKeys();
+            const response = await axios.get(apiURL, null);
+            const result = response.data;
+            console.log(result);
+            setToDos(result);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     const openModal = () => {
         setModalVisible(true);
@@ -37,11 +62,84 @@ export function AddTaskScreen(){
         hideTimePicker();
     };
 
+    const convertTime = (timeStr) => {
+        const [hours, minutes] = timeStr.split(':');
+        const formattedTime = `${hours % 12 || 12}:${minutes} ${hours >= 12 ? 'PM' : 'AM'}`;
+        return formattedTime;
+    }
+
+    const addTask = async (data) => {
+        const apiURL = BASE_URL + "/api/todolist/addTask";
+        try {
+            await updateKeys();
+            const response = await axios.post(apiURL, data);
+            const result = response.data;
+            console.log(result);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const convertTimeBack = (timeStr) => {
+        const [hhmm, ampm] = timeStr.split(' ');
+        const [hours, minutes] = hhmm.split(':');
+
+        let hours24 = parseInt(hours, 10);
+        if (ampm.toLowerCase() === 'pm' && hours24 < 12) {
+            hours24 += 12;
+        } else if (ampm.toLowerCase() === 'am' && hours24 === 12) {
+            hours24 = 0;
+        }
+
+        const seconds = '00';
+        const formattedTime = `${hours24.toString().padStart(2, '0')}:${minutes}:${seconds}`;
+        return formattedTime;
+    }
+
+    const refreshScreen = () => {
+        setToDos((prevTodos) => [...prevTodos]); // This will trigger a re-render
+    };
+
+    const save = () => {
+        if (task === '' || description === '' || time === '') {
+            Alert.alert('Please fill in all the fields');
+        } else {
+            const formattedTime = convertTimeBack(time);
+            const data = {
+                task: task,
+                taskDescription: description,
+                time: formattedTime,
+                date: formattedDate,
+                status: 'NotCompleted'
+            }
+            addTask(data).then(r => console.log(r));
+            setToDos([...toDos, addTask(data)]);
+            closeModal();
+            refreshScreen();
+        }
+    }
+
     return(
         <SafeAreaView>
             <ToDoHeader screen={"ToDoList"}/>
             <View>
-                <Text>Add Task Screen</Text>
+                <Text className={"text-center font-bold text-2xl"}>Task List</Text>
+                <FlatList
+                    data={toDos}
+                    style={{height: "75%"}}
+                    keyExtractor={(item) => item.toDoID}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity>
+                            <View className={"p-2 px-4 border rounded-xl mx-8 my-2 border-primary"}>
+                                <View className={"flex flex-row justify-between"}>
+                                    <Text className={"font-medium"}>{item.task}</Text>
+                                    <Text className={"font-light"}>At {convertTime(item.time)}</Text>
+                                </View>
+                                <Text>Description : {item.taskDescription}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                />
             </View>
             <View className={"px-16"}>
                 <FilledButton title={"Add New Task"} icon={"plus"} onPress={openModal}/>
@@ -90,7 +188,7 @@ export function AddTaskScreen(){
                         </View>
                         <View className={"flex flex-row"}>
                             <View className={"w-1/2"}>
-                                <FilledButton title="Add Task" icon="plus" />
+                                <FilledButton title="Add Task" icon="plus" onPress={save}/>
                             </View>
                             <View className={"w-1/2"}>
                                 <FilledButton title="Cancel" icon="close" onPress={closeModal} />
